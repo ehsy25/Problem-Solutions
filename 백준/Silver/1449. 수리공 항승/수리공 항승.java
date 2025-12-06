fde10e20a9c54466a696dfda654e3b92fdf7a44e
Eric H Youn <115512510+ehsy25@users.noreply.github.com>
2025-12-07 01:15:55 +0900

    [Silver III] Title: 수리공 항승, Time: 156 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // 물이 새는 곳의 개수
+        int l = sc.nextInt(); // 테이프의 길이
+        int[] leaks = new int[n + 1];
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            leaks[i] = sc.nextInt(); // 물이 새는 곳
+        }
+
+        Arrays.sort(leaks);
+
+        int idx = 1;
+
+        while (idx <= n) {
+            count++;
+            double limit = leaks[idx] + l - 0.5;
+            while (idx <= n && leaks[idx] <= limit) {
+                idx++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
