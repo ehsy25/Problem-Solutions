a06e1f70aba1eb1d90cba1a32666c8708f82521d
Eric Y <eyoun17@gmail.com>
2025-12-14 23:55:03 +0900

    retried n and m

---
+++ Problems/src/main/java/com/E/unsolved_problems/BJ15649/Main.java
@@ -0,0 +1,47 @@
+package com.E.unsolved_problems.BJ15649;
+
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int m;
+    static int [] ns;
+    static boolean [] v;
+    static int[] recursiveN;
+    public static void main(String[] args) {
+// 백준 문제 n과m 다시 풀어보기
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        ns = new int[n+1];
+        v = new boolean[n+1];
+        recursiveN = new int[n+1];
+
+        for(int i = 0; i <= n; i++){
+           ns[i] = i; 
+        }
+        
+        //재귀 함수 다시 만들기
+        re(0);
+
+    }
+
+    public static void re(int depth){
+        if(depth == m){
+            for(int i = 0; i < m; i++){
+                System.out.print(recursiveN[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+            if(depth < m){
+            for(int i = 1; i <= n; i++){
+            recursiveN[depth] = i;
+            v[i] = true;
+            re(depth++);
+            v[i] = false;   
+            }
+        }
+}
+}
