757a0ec4b19f6249f8310fefa7158c2e3366381c
Eric Y <eyoun17@gmail.com>
2025-12-22 23:55:39 +0900

    BJ2839 started

--- Problems/src/main/java/com/E/Main.java
+++ Problems/src/main/java/com/E/Main.java
@@ -13,25 +13,23 @@ public class Main {
         // System.setIn(new FileInputStream(
         // "C:\\Users\\Eric\\Documents\\PP\\Problem_Solutions\\Problems\\src\\main\\java\\com\\E\\input.txt"));
 
+        //BJ2839 설탕 배달
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int count = 0;
-        while (n != 1) {
-            if (n % 3 == 0 ) {
-                n = n / 3;
-                count++;
-                System.out.println("n/3");
-            } else if (n % 2 == 0 && (n-1)%3 != 0) {
-                n = n / 2;
-                count++;
-                                System.out.println("n/2");
 
-            } else {
-                n -= 1;
+        while(n > 0) {
+            if(n > 5){
+                n /= 5;
                 count++;
-                                System.out.println("n-1");
+            }
+            else {
+                n /= 3;
+                count++;
             }
         }
+
         System.out.println(count);
     }
 }
