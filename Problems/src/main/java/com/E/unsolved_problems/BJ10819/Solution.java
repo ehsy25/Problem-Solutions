901d390de3f89afaf4bca74c0152896e64f22eec
Eric Y <eyoun17@gmail.com>
2026-01-17 23:30:01 +0900

    added solution for BJ10819

--- Problems/src/main/java/com/E/unsolved_problems/BJ10819/Solution.java
+++ Problems/src/main/java/com/E/unsolved_problems/BJ10819/Solution.java
@@ -1,13 +1,10 @@
-package com.E;
+package com.E.unsolved_problems.BJ10819;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
-public class Main {
+public class Solution {
 
     static int n;
     static int[] arr;        // 입력 배열
